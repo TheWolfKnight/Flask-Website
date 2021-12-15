30a0815e9d7eaf5fce94c0b40736e6e1ef5534bf
TheWolfKnight <philipalexknudsen@gmail.com>
2021-12-15 09:50:28 +0100

    commit #4

--- roadmap.md
+++ roadmap.md
@@ -2,18 +2,18 @@
 # TODO:
 
 - Forum:
-	- Login/Logout/Sign-up
-	- Making Post
-		- See/Comment for other users
-		- Could be like reddit?
-	- Text oneanother
-	- Theme:
-		- Dark
-		- Simple
-		- Black/Red/Grays
-		- Straigt forward
-	- A pixel
-	- Logo
+    - Login/Logout/Sign-up
+    - Making Post
+        - See/Comment for other users
+        - Could be like reddit?
+    - Text oneanother
+    - Theme:
+        - Dark
+        - Simple
+        - Black/Red/Grays
+        - Straigt forward
+    - A pixel
+    - Logo
 
 # Void:
 
--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 Auth = Blueprint('Auth', __name__)
 
-@Auth.route('/login')
-def login():
+@Auth.route('/login', methods=['GET'])
+def render_login() -> str:
     return render_template('user_input_tmp.html', type="Login")
 
 
+@Auth.route('/login', methods=['POST'])
+def login():
+    pass
+
 @Auth.route('/logout')
-def logout():
+def logout() -> str:
     content: str = """
 Could not find the page you were looking for.
 Please make sure your link is valid, and it was typed correctly.
@@ -16,6 +20,12 @@ Please make sure your link is valid, and it was typed correctly.
     return render_template('error.html', error_code="404", error_content=content)
 
 
-@Auth.route('/sign-up')
-def sign_up():
+@Auth.route('/sign-up', methods=['GET'])
+def render_sign_up() -> str:
     return render_template('user_input_tmp.html', type="Sign Up")
+
+
+@Auth.route('/sign-up', methods=['POST'])
+def sign_up() -> str:
+    pass
+
